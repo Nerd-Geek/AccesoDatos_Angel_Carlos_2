8653494d16436cfa2f404cbdd9799ac42d5bacb0
Carlos Moreno <carlos.morenoluengos@gmail.com>
2021-10-16 21:53:27 +0200

    Arregladas las clases reader, agregado main y reestructura del proyecto
    - Clases reader arregladas, juntadas y primeras versiones terminadas.
    - Agregado código al Main que ejecuta el Reader (comprobación de código)
    - Se ha reestructurado el proyecto. Ahora la carpeta data fue eliminada y su contenido fue desplazado a /resources

--- src/main/java/com/angcar/App.java
+++ src/main/java/com/angcar/App.java
@@ -1,7 +1,68 @@
 package com.angcar;
 
+import com.angcar.io.ReaderFiles;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) {
 
+        String WORKING_DIRECTORY = System.getProperty("user.dir");
+
+        /**
+         * TODO: refactorizar nombre : argsTemporal por args &&
+         * TODO: ELIMINAR ESTA PARTE CUANDO TERMINAMOS EL PROYECTO
+         * ESTO LO QUE HACE ES PONER MANUALMENTE LOS ARGUMENTOS
+         * ASÍ NO TENEMOS QUE CREAR Y EJECUTAR EL .JAR CADA VEZ
+         */
+        String[] argsTemporal = new String[2];
+        argsTemporal[0] = "MADRID";
+        argsTemporal[1] = "out";
+
+        /**
+         * Detectar el número de argumentos y procesarlos
+         */
+        if (argsTemporal.length >= 2 && (argsTemporal.length % 2 == 0)) {
+            List<String[]> pares = IntStream.iterate(0, i -> i += 2).limit(argsTemporal.length/2)
+                    .mapToObj(n -> new String[] { argsTemporal[n], argsTemporal[n + 1] }).collect(Collectors.toList());
+
+            pares.stream().forEach(pair -> {
+                String ciudad = pair[0]; //Argumento ciudad
+                Path path = Paths.get(WORKING_DIRECTORY + File.separator + pair[1]); //Archivo
+
+                System.out.println(ciudad.toString());
+                System.out.println(path.toString());
+
+                //Lee los datos
+                //MeteoReader.readDataOfPathMeteorologia().stream().forEach(System.out::println);
+
+
+                //MeteoReader.readDataOfPathContaminacion();
+                //MeteoReader.readDataOfPathUbicacionEstaciones();
+                //MeteoReader.readDataOfPathZonasMunicipio();
+
+                //MeteoReader.readDataOfPathZonasMunicipio().stream().forEach(System.out::println);
+
+                ReaderFiles.readDataOfPathContaminacion().stream().forEach(System.out::println);
+
+                //MeteoReader.readDataOfPathZonasMunicipio().ifPresent((list) -> {
+                    //servicio = new ServicioClimatologia(list);
+                    //imprimirInformeMeteorologico(ld.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+
+                //    System.out.println(list);
+
+                //});
+            });
+
+        } else {
+            //TODO: Comprobar que el ejemplo funcione
+            System.err.println(
+                    "Error de sintaxis:\nDebes de utilizar el siguiente formato:\njava -jar meteo.jar <ciudad> <fichero>\n\nEjemplo: java -jar meteo.jar fuenlabrada ciudad_aire_zonas.csv");
+        }
     }
 }
---
+++ src/main/java/com/angcar/io/ReaderFiles.java
@@ -0,0 +1,217 @@
+package com.angcar.io;
+
+import com.angcar.model.Contaminacion;
+import com.angcar.model.Meteorizacion;
+import com.angcar.model.UbicacionEstaciones;
+import com.angcar.model.ZonasMunicipio;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReaderFiles {
+
+    // Localización csv´s
+    private static final String PATH_ZONAS = "src/main/resources/calidad_aire_zonas.csv";
+    private static final String PATH_UBICA_ESTACIONES = "src/main/resources/calidad_aire_estaciones.csv";
+    private static final String PATH_METEO = "src/main/resources/calidad_aire_datos_meteo_mes.csv";
+    private static final String PATH_CONTAMINACION = "src/main/resources/calidad_aire_datos_mes.csv";
+
+
+    //Método para leer los ficheros e insertar los datos en la clase Meteorizacion
+    public static Optional<List<Meteorizacion>> readDataOfPathMeteorologia() {
+
+        Path path = Paths.get(String.valueOf(PATH_METEO));
+
+        if (Files.exists(path)) {
+            try (Stream<String> stream = Files.lines(path, Charset.forName("Cp1252"))) {
+                return Optional.of(stream
+                        .map(s -> s.split(";"))
+                        .map(splitted -> {
+
+                            String provincia = splitted[0];
+                            String municipio = splitted[1];
+                            String estacion = splitted[2];
+                            String magnitud = splitted[3];
+                            String punto_muestreo = splitted[4];
+                            String ano = splitted[5];
+                            String mes = splitted[6];
+                            String dia = splitted[7];
+
+
+                            ArrayList horas = new ArrayList();
+                            ArrayList validacion = new ArrayList();
+
+                            for (int n = 8; n < splitted.length; n++) {
+                                if (n % 2 == 0) {
+                                    horas.add(splitted[n]);
+                                }else {
+                                    validacion.add(splitted[n]);
+                                }
+                            }
+
+                            return new Meteorizacion(provincia, municipio, estacion, magnitud, punto_muestreo, ano, mes, dia, horas, validacion);
+                        })
+                        .collect(Collectors.toList()));
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    //Método para leer los ficheros e insertar los datos en la clase Contaminacion
+    public static Optional<List<Contaminacion>> readDataOfPathContaminacion() {
+        Path path = Paths.get(String.valueOf(PATH_CONTAMINACION));
+
+        if (Files.exists(path)) {
+            try (Stream<String> stream = Files.lines(path, Charset.forName("Cp1252"))) {
+                return Optional.of(stream
+                        .map(s -> s.split(";"))
+                        .map(splitted -> {
+
+                            String provincia = splitted[0];
+                            String municipio = splitted[1];
+                            String estacion = splitted[2];
+                            String magnitud = splitted[3];
+                            String punto_muestreo = splitted[4];
+                            String ano = splitted[5];
+                            String mes = splitted[6];
+                            String dia = splitted[7];
+
+                            ArrayList horas = new ArrayList();
+                            ArrayList validacion = new ArrayList();
+
+                            for (int n = 8; n < splitted.length; n++) {
+                                if (n % 2 == 0) {
+                                    horas.add(splitted[n]);
+                                }else {
+                                    validacion.add(splitted[n]);
+                                }
+                            }
+
+                            return new Contaminacion(provincia, municipio, estacion, magnitud, punto_muestreo, ano, mes, dia, horas, validacion);
+
+                        })
+                        .collect(Collectors.toList()));
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    //Método para leer los ficheros e insertar los datos en la clase UbicacionEstaciones
+    public static Optional<List<UbicacionEstaciones>> readDataOfPathUbicacionEstaciones() {
+        Path path = Paths.get(PATH_UBICA_ESTACIONES);
+
+        if (Files.exists(path)) {
+            try (Stream<String> stream = Files.lines(path, Charset.forName("Cp1252"))) {
+                return Optional.of(stream
+                        .map(s -> s.split(";"))
+                        .map(splitted -> {
+                            String estacion_codigo = splitted[0];
+                            String zona_calidad_aire_descripcion = splitted[1];
+                            String estacion_municipio = splitted[2];
+                            String estacion_fecha_alta = splitted[3];
+                            String estacion_tipo_area = splitted[4];
+                            String estacion_tipo_estacion = splitted[5];
+                            String estacion_subarea_rural = splitted[6];
+                            String estacion_direccion_postal = splitted[7];
+                            String estacion_coord_UTM_ETRS89_x = splitted[8];
+                            String estacion_coord_UTM_ETRS89_y = splitted[9];
+                            String estacion_coord_longitud = splitted[10];
+                            String estacion_coord_latitud = splitted[11];
+                            String estacion_altitud = splitted[12];
+                            String estacion_analizador_NO = splitted[13];
+                            String estacion_analizador_NO2 = splitted[14];
+                            String estacion_analizador_PM10 = splitted[15];
+                            String estacion_analizador_PM2_5 = splitted[16];
+                            String estacion_analizador_O3 = splitted[17];
+                            String estacion_analizador_TOL = splitted[18];
+                            String estacion_analizador_BEN = splitted[19];
+                            String estacion_analizador_XIL = splitted[20];
+                            String estacion_analizador_CO = splitted[21];
+                            String estacion_analizador_SO2 = splitted[22];
+                            String estacion_analizador_HCT = splitted[23];
+                            String estacion_analizador_HNM = splitted[24];
+
+                            return new UbicacionEstaciones(
+                                    estacion_codigo
+                                    , zona_calidad_aire_descripcion
+                                    , estacion_municipio
+                                    , estacion_fecha_alta
+                                    , estacion_tipo_area
+                                    , estacion_tipo_estacion
+                                    , estacion_subarea_rural
+                                    , estacion_direccion_postal
+                                    , estacion_coord_UTM_ETRS89_x
+                                    , estacion_coord_UTM_ETRS89_y
+                                    , estacion_coord_longitud
+                                    , estacion_coord_latitud
+                                    , estacion_altitud
+                                    , estacion_analizador_NO
+                                    , estacion_analizador_NO2
+                                    , estacion_analizador_PM10
+                                    , estacion_analizador_PM2_5
+                                    , estacion_analizador_O3
+                                    , estacion_analizador_TOL
+                                    , estacion_analizador_BEN
+                                    , estacion_analizador_XIL
+                                    , estacion_analizador_CO
+                                    , estacion_analizador_SO2
+                                    , estacion_analizador_HCT
+                                    , estacion_analizador_HNM
+                            );
+                        })
+                        .collect(Collectors.toList()));
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+
+    }
+
+    //Método para leer los ficheros e insertar los datos en la clase ZonasMunicipio
+    public static Optional<List<ZonasMunicipio>> readDataOfPathZonasMunicipio() {
+
+        Path path = Paths.get(PATH_ZONAS);
+
+        if (Files.exists(path)) {
+            try (Stream<String> stream = Files.lines(path, Charset.forName("Cp1252"))) {
+                return Optional.of(stream
+                        .map(s -> s.split(";"))
+                        .map(splitted -> {
+
+                            String zona_calidad_aire_codigo = splitted[0];
+                            String zona_calidad_aire_descripcion = splitted[1];
+                            String zona_calidad_aire_municipio = splitted[2];
+
+                            return new ZonasMunicipio(zona_calidad_aire_codigo,
+                                    zona_calidad_aire_descripcion, zona_calidad_aire_municipio);
+
+                        })
+                        .collect(Collectors.toList()));
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+}
