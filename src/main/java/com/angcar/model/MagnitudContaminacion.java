2875e66e7fb16715e3bbe552944e39da5921ec16
Carlos Moreno <carlos.morenoluengos@gmail.com>
2021-10-18 12:57:40 +0200

    Modificación clases modelos y agregadas Maginitudes
    Ha sido modificada la clase modelo, se han agregado dos clases nuevas MagnitudMeteorizacion y MaginitudContaminacion con sus atributos (se debería crear los métodos correspondientes para insertar los datos de las tablas PDF)

---
+++ src/main/java/com/angcar/model/MagnitudContaminacion.java
@@ -0,0 +1,16 @@
+package com.angcar.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MagnitudContaminacion {
+
+    private int codigo_maginitud;
+    private String descripcion_magnitud;
+    private int codigo_tecnica_medida;
+    private String descipcion_tecnica_medida;
+    private String unidad;
+    private String descripcion_unidad;
+}
