0cd46beaa64d82464160d6c5172f6b4ee91d7d7c
Carlos Moreno <carlos.morenoluengos@gmail.com>
2021-10-22 18:23:03 +0200

    Agregadas las mediciones min y max
    Se ha agregado las mediciones min y max según el código de medición pasado

--- src/main/java/com/angcar/ProcesamientoDatos.java
+++ src/main/java/com/angcar/ProcesamientoDatos.java
@@ -48,7 +48,6 @@ public class ProcesamientoDatos {
             Utils.inicializarDatos();
 
 
-
             //FILTRAMOS POR ESTACIONES LOS ARCHIVOS
             List<UbicacionEstaciones> listaEstaciones = Utils.filtrarPorCiudad(pair[0]);
             if (listaEstaciones.size() == 0) {
@@ -70,118 +69,154 @@ public class ProcesamientoDatos {
             //INFORMACIÓN METEOROLÓGICA//
             /////////////////////////////
             //TEMPERATURA
-            System.out.println(MeteoService.temperaturaMediaMensual(listaMeteorizacion,83)); //media mes
-                //TODO: Momento y temperatura máxima
-                //TODO: Momento y temperatura mínima
+            System.out.println("Datos temperatura:");
+            System.out.println(MeteoService.medicionMedia(listaMeteorizacion,83)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,83)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,83)); //minima mes
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //RADIACIÓN SOLAR
-            System.out.println(MeteoService.temperaturaMediaMensual(listaMeteorizacion,88)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaMeteorizacion,88)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,88)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,88)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
                 //PRECIPITACIÓN
-            System.out.println(MeteoService.temperaturaMediaMensual(listaMeteorizacion,89)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaMeteorizacion,89)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,89)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,89)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
                 //HUMEDAD
-            System.out.println(MeteoService.temperaturaMediaMensual(listaMeteorizacion,86)); //media mes
-                //TODO: Momento y temperatura máxima
+            System.out.println(MeteoService.medicionMedia(listaMeteorizacion,86)+ " Media"); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,86)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,86)); //minima mes
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
                 //VELOCIDAD DEL VIENTO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaMeteorizacion,81)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaMeteorizacion,81)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,81)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,81)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
-
+            System.out.println("Contaminación");
             /////////////////////////////
             //INFORMACIÓN CONTAMINACIÓN//
             /////////////////////////////
             //DIÓXIDO DE AZUFRE
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,1)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,1)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,1)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,1)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //MONÓXIDO DE CARBONO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,6)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,6)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,6)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,6)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //MONÓXIDO DE NITRÓGENO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,7)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,7)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,7)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,7)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //DIÓXIDO DE NITRÓGENO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,8)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,8)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,8)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,8)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //PARTÍCULAS EN SUSPENSIÓN < PM2,5
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,9)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,9)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,9)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,9)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //PARTÍCULAS EN SUSPENSIÓN < PM10
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,10)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,10)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,10)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,10)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //ÓXIDOS DE NITRÓGENO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,12)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,12)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,12)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,12)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //OZONO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,14)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,14)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,14)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,14)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //TOLUENO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,20)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,20)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,20)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,20)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //BLACK CARBON
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,22)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,22)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,22)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,22)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //BECENO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,30)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,30)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,30)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,30)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //HIDROCARBUROS TOTALES
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,42)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,42)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,42)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,42)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //HIDROCARBUROS NO METÁNICOS
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,44)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,44)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,44)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,44)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
 
             //METAPARAXILENO
-            System.out.println(MeteoService.temperaturaMediaMensual(listaContaminacion,431)); //media mes
+            System.out.println(MeteoService.medicionMedia(listaContaminacion,431)); //media mes
+            System.out.println(MeteoService.medicionMinima(listaMeteorizacion,431)); //maxima mes
+            System.out.println(MeteoService.medicionMaxima(listaMeteorizacion,431)); //minima mes
                 //TODO: Momento y temperatura máxima
                 //TODO: Momento y temperatura mínima
                 //TODO: Gráfica de la evolución de la temperatura a nivel mensual
